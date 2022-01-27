6537e14a29952988af4a6007283089b2a6b17fad
Dimitris Pappas <dpappas@prudea.com>
2022-01-27 20:15:59 +0200

    Day 19 - Part 1

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,250 +1,302 @@
-var data = @"[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
-[[[5,[2,8]],4],[5,[[9,9],0]]]
-[6,[[[6,2],[5,6]],[[7,6],[4,7]]]]
-[[[6,[0,7]],[0,9]],[4,[9,[9,0]]]]
-[[[7,[6,4]],[3,[1,3]]],[[[5,5],1],9]]
-[[6,[[7,3],[3,2]]],[[[3,8],[5,7]],4]]
-[[[[5,4],[7,7]],8],[[8,3],8]]
-[[9,3],[[9,9],[6,[4,9]]]]
-[[2,[[7,7],7]],[[5,8],[[9,3],[0,2]]]]
-[[[[5,2],5],[8,[3,7]]],[[5,[7,5]],[4,4]]]";
+var data = @"--- scanner 0 ---
+404,-588,-901
+528,-643,409
+-838,591,734
+390,-675,-793
+-537,-823,-458
+-485,-357,347
+-345,-311,381
+-661,-816,-575
+-876,649,763
+-618,-824,-621
+553,345,-567
+474,580,667
+-447,-329,318
+-584,868,-557
+544,-627,-890
+564,392,-477
+455,729,728
+-892,524,684
+-689,845,-530
+423,-701,434
+7,-33,-71
+630,319,-379
+443,580,662
+-789,900,-551
+459,-707,401
 
-var lines = data.Split('\n');
+--- scanner 1 ---
+686,422,578
+605,423,415
+515,917,-361
+-336,658,858
+95,138,22
+-476,619,847
+-340,-569,-846
+567,-361,727
+-460,603,-452
+669,-402,600
+729,430,532
+-500,-761,534
+-322,571,750
+-466,-666,-811
+-429,-592,574
+-355,545,-477
+703,-491,-529
+-328,-685,520
+413,935,-424
+-391,539,-444
+586,-435,557
+-364,-763,-893
+807,-499,-711
+755,-354,-619
+553,889,-390
 
-var max = Int64.MinValue;
-for (int i = 0; i < lines.Length; ++i)
+--- scanner 2 ---
+649,640,665
+682,-795,504
+-784,533,-524
+-644,584,-595
+-588,-843,648
+-30,6,44
+-674,560,763
+500,723,-460
+609,671,-379
+-555,-800,653
+-675,-892,-343
+697,-426,-610
+578,704,681
+493,664,-388
+-671,-858,530
+-667,343,800
+571,-461,-707
+-138,-166,112
+-889,563,-600
+646,-828,498
+640,759,510
+-630,509,768
+-681,-892,-333
+673,-379,-804
+-742,-814,-386
+577,-820,562
+
+--- scanner 3 ---
+-589,542,597
+605,-692,669
+-500,565,-823
+-660,373,557
+-458,-679,-417
+-488,449,543
+-626,468,-788
+338,-750,-386
+528,-832,-391
+562,-778,733
+-938,-730,414
+543,643,-506
+-524,371,-870
+407,773,750
+-104,29,83
+378,-903,-323
+-778,-728,485
+426,699,580
+-438,-605,-362
+-469,-447,-387
+509,732,623
+647,635,-688
+-868,-804,481
+614,-800,639
+595,780,-596
+
+--- scanner 4 ---
+727,592,562
+-293,-554,779
+441,611,-461
+-714,465,-776
+-743,427,-804
+-660,-479,-426
+832,-632,460
+927,-485,-438
+408,393,-506
+466,436,-512
+110,16,151
+-258,-428,682
+-393,719,612
+-211,-452,876
+808,-476,-593
+-575,615,604
+-485,667,467
+-680,325,-822
+-627,-443,-432
+872,-547,-609
+833,512,582
+807,604,487
+839,-516,451
+891,-625,532
+-652,-548,-490
+30,-46,-14";
+
+var dataScanners = data.Split("\n\n");
+
+var scanners = new Dictionary<string, List<Probe>>();
+
+var probesList = new List<List<Probe>>();
+
+var finalProbes = new List<Probe>();
+
+foreach (var s in dataScanners)
 {
-    for (int j = 0; j < lines.Length; j++)
-    {
-        if (i == j)
-            continue;
+    var probes = new List<Probe>();
+    var probeLines = s.Split('\n');
 
-        var root = new TreeNode(null)
-        {
-            Left = ParseLine(lines[i]),
-            Right = ParseLine(lines[j])
-        };
-        
-        root.Left.Parent = root;
-        root.Right.Parent = root;
-        
-        // Console.Write("after addition: ");
-        // PrintTree(root);
-        // Console.WriteLine();
+    for (int i = 1; i < probeLines.Length; i++)
+        probes.Add(new Probe(probeLines[i]));
 
-        while (true)
-        {
-            bool exploded = false;
-            bool splited = false;
-
-            while (PerformExplode(root))
-            {
-                exploded = true;
-                // Console.Write("after explode: ");
-                // PrintTree(root);
-                // Console.WriteLine();
-            }
-
-            splited = PerformSplit(root);
-            if (splited)
-            {
-                // Console.Write("after split: ");
-                // PrintTree(root);
-                // Console.WriteLine();
-            }
-
-            if (!(exploded || splited))
-                break;
-        }
-        
-        // PrintTree(root);
-        // Console.WriteLine();
-
-        var result = CalculateResult(root);
-        if (result > max)
-            max = result;
-    }
+    scanners[probeLines[0]] = probes;
+    probesList.Add(probes);
 }
 
-Console.WriteLine(max);
+finalProbes.AddRange(probesList[0]);
+
+probesList.RemoveAt(0);
+
+var pos = 0;
+while (probesList.Count > 0)
+{
+    bool matched = false;
+
+    // apply transformation
+    // 24 transformations
+    for (int t = 0; t < 24 && !matched; ++t)
+    {
+        var newProbes = Transform(probesList[pos], t);
+
+        // try to match every probe with every final probe
+        foreach (var probe in newProbes)
+        {
+            foreach (var finalProbe in finalProbes)
+            {
+                // get sensor pos
+                // sensor pos = (p1.x - p2.x, p1.y - p2.y, p1.z - p2.z)
+                var sensorPos = (finalProbe.X - probe.X, finalProbe.Y - probe.Y, finalProbe.Z - probe.Z);
+
+                // offset all probes
+                // p1_pos + sensor_pos = p2_pos
+                var offset = Offset(newProbes, sensorPos);
+
+                // check how many match
+                var matches = offset.Count(p => finalProbes.Any(f => f.X == p.X && f.Y == p.Y && f.Z == p.Z));
+
+                if (matches >= 12)
+                {
+                    Console.WriteLine(matches);
+                    finalProbes.AddRange(
+                        offset.Where(p => finalProbes.All(f => f.X != p.X || f.Y != p.Y || f.Z != p.Z)));
+                    probesList.RemoveAt(pos);
+                    matched = true;
+                    break;
+                }
+            }
+
+            if (matched)
+                break;
+        }
+    }
+
+    if (matched == false)
+    {
+        pos++;
+    }
+
+    if (pos >= probesList.Count)
+        pos = 0;
+}
+
+Console.WriteLine(finalProbes.Count);
+
+List<Probe> Transform(List<Probe> list, int transformation)
+{
+    return list.Select(i => i.Transform(transformation)).ToList();
+}
+
+List<Probe> Offset(IEnumerable<Probe> list, (int, int, int) offset)
+{
+    return list.Select(i => i.Offset(offset)).ToList();
+}
+
 
 return 0;
 
-long CalculateResult(TreeNode start)
+struct Probe
 {
-    if (start.Value != null)
-        return start.Value ?? 0;
+    private static int _counter = 0;
 
-    return CalculateResult(start.Left) * 3 + CalculateResult(start.Right) * 2;
-}
+    public int Id { get; }
+    public int X { get; }
+    public int Y { get; }
+    public int Z { get; }
 
-TreeNode ParseLine(string line)
-{
-    var tree = new TreeNode(null);
-    var current = tree;
-
-
-    foreach (var c in line)
+    private Probe(int x, int y, int z, int id)
     {
-        if (c == '[')
+        this.X = x;
+        this.Y = y;
+        this.Z = z;
+        this.Id = id;
+    }
+
+    public Probe(string dimensions)
+    {
+        var parts = dimensions.Split(',');
+        Id = _counter++;
+        X = int.Parse(parts[0]);
+        Y = int.Parse(parts[1]);
+        Z = int.Parse(parts[2]);
+    }
+
+    public Probe Transform(int transformation)
+    {
+        return transformation switch
         {
-            current.Left = new TreeNode(current);
-            current = current.Left;
-        }
-        else if (c >= '0' && c <= '9')
-        {
-            current.Value = c - '0';
-        }
-        else if (c == ',')
-        {
-            current.Parent.Right = new TreeNode(current.Parent);
-            current = current.Parent.Right;
-        }
-        else if (c == ']')
-        {
-            current = current.Parent;
-        }
+            //
+            0 => new Probe(X, Y, Z, Id),
+            1 => new Probe(-Y, X, Z, Id),
+            2 => new Probe(-X, -Y, Z, Id),
+            3 => new Probe(Y, -X, Z, Id),
+            //
+            4 => new Probe(X, -Z, Y, Id),
+            5 => new Probe(Z, X, Y, Id),
+            6 => new Probe(-X, Z, Y, Id),
+            7 => new Probe(-Z, -X, Y, Id),
+            //
+            8 => new Probe(X, -Y, -Z, Id),
+            9 => new Probe(Y, X, -Z, Id),
+            10 => new Probe(-X, Y, -Z, Id),
+            11 => new Probe(-Y, -X, -Z, Id),
+            //
+            12 => new Probe(X, Z, -Y, Id),
+            13 => new Probe(-Z, X, -Y, Id),
+            14 => new Probe(-X, -Z, -Y, Id),
+            15 => new Probe(Z, -X, -Y, Id),
+            //
+            16 => new Probe(Z, Y, -X, Id),
+            17 => new Probe(-Y, Z, -X, Id),
+            18 => new Probe(-Z, -Y, -X, Id),
+            19 => new Probe(Y, -Z, -X, Id),
+            //
+            20 => new Probe(-Z, Y, X, Id),
+            21 => new Probe(-Y, -Z, X, Id),
+            22 => new Probe(Z, -Y, X, Id),
+            23 => new Probe(Y, Z, X, Id),
+            _ => throw new Exception("Unknown transformation")
+        };
     }
 
-    return tree;
-}
-
-bool PerformExplode(TreeNode start, int depth = 1)
-{
-    if (depth > 4 && start.Value == null) // has children
+    public Probe Offset((int, int, int) offset)
     {
-        if (start.Left.Value == null)
-            throw new Exception("Left of depth 5 is not value...");
-
-        if (start.Right.Value == null)
-            throw new Exception("Right of depth 5 is not value...");
-
-        // find  first left value (if any)
-        // climb up if this is left child
-        var current = start;
-        var parent = start.Parent;
-        while (parent != null && parent.Left == current)
-        {
-            current = parent;
-            parent = current.Parent;
-        }
-
-        // climb down to rightmost child of left child of parent
-        if (parent != null)
-        {
-            current = parent.Left;
-
-            while (current.Value == null && current.Right != null)
-                current = current.Right;
-
-            current.Value += start.Left.Value;
-        }
-
-
-        // find first right value (if any)
-        // climb up if this is right child
-        current = start;
-        parent = start.Parent;
-        while (parent != null && parent.Right == current)
-        {
-            current = parent;
-            parent = current.Parent;
-        }
-
-        // climb down to leftmost child of right child of parent
-        if (parent != null)
-        {
-            current = parent.Right;
-
-            while (current.Value == null && current.Left != null)
-                current = current.Left;
-
-            current.Value += start.Right.Value;
-        }
-
-        start.Left = null;
-        start.Right = null;
-        start.Value = 0;
-
-        return true;
+        return new Probe(X + offset.Item1, Y + offset.Item2, Z + offset.Item3, Id);
     }
-    
-    if (start.Left != null)
+
+    public double Distance(Probe other)
     {
-        if (PerformExplode(start.Left, depth + 1))
-            return true;
+        return Math.Round(Math.Sqrt(Math.Pow(other.X - X, 2) + Math.Pow(other.Y - Y, 2) + Math.Pow(other.Z - Z, 2)), 6);
     }
-
-    if (start.Right != null)
-    {
-        if (PerformExplode(start.Right, depth + 1))
-            return true;
-    }
-
-    // action not found, continue traversing tree
-    return false;
-}
-
-bool PerformSplit(TreeNode start)
-{
-    // split
-    if (start.Value != null && start.Value >= 10)
-    {
-        start.Left = new TreeNode(start, start.Value / 2);
-        start.Right = new TreeNode(start, (start.Value + 1) / 2);
-        start.Value = null;
-
-        return true;
-    }
-
-    if (start.Left != null)
-    {
-        if (PerformSplit(start.Left))
-            return true;
-    }
-
-    if (start.Right != null)
-    {
-        if (PerformSplit(start.Right))
-            return true;
-    }
-
-    // action not found, continue traversing tree
-    return false;
-}
-
-void PrintTree(TreeNode start)
-{
-    if (start.Value != null)
-    {
-        Console.Write(start.Value);
-        return;
-    }
-
-    Console.Write('[');
-    PrintTree(start.Left);
-    Console.Write(',');
-    PrintTree(start.Right);
-    Console.Write(']');
-}
-
-class TreeNode
-{
-    public TreeNode(TreeNode? parent)
-    {
-        Parent = parent;
-    }
-
-    public TreeNode(TreeNode? parent, long? value)
-    {
-        Parent = parent;
-        Value = value;
-    }
-
-    public long? Value { get; set; } = null;
-    public TreeNode? Left { get; set; }
-
-    public TreeNode? Right { get; set; }
-
-    public TreeNode? Parent { get; set; }
 }
