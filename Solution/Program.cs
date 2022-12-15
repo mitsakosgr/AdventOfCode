55d548d43efe5c80886b0d2266cd226c4082d76c
Dimitris Pappas <dpappas@prudea.com>
2022-12-15 15:55:20 +0200

    Day 12 - Part 1

--- Solution/Program.cs
+++ Solution/Program.cs
@@ -1,143 +1,154 @@
-using System.ComponentModel;
-using System.Numerics;
-using System.Text.RegularExpressions;
-
-var input = @"Monkey 0:
-  Starting items: 99, 67, 92, 61, 83, 64, 98
-  Operation: new = old * 17
-  Test: divisible by 3
-    If true: throw to monkey 4
-    If false: throw to monkey 2
-
-Monkey 1:
-  Starting items: 78, 74, 88, 89, 50
-  Operation: new = old * 11
-  Test: divisible by 5
-    If true: throw to monkey 3
-    If false: throw to monkey 5
-
-Monkey 2:
-  Starting items: 98, 91
-  Operation: new = old + 4
-  Test: divisible by 2
-    If true: throw to monkey 6
-    If false: throw to monkey 4
-
-Monkey 3:
-  Starting items: 59, 72, 94, 91, 79, 88, 94, 51
-  Operation: new = old * old
-  Test: divisible by 13
-    If true: throw to monkey 0
-    If false: throw to monkey 5
-
-Monkey 4:
-  Starting items: 95, 72, 78
-  Operation: new = old + 7
-  Test: divisible by 11
-    If true: throw to monkey 7
-    If false: throw to monkey 6
-
-Monkey 5:
-  Starting items: 76
-  Operation: new = old + 8
-  Test: divisible by 17
-    If true: throw to monkey 0
-    If false: throw to monkey 2
-
-Monkey 6:
-  Starting items: 69, 60, 53, 89, 71, 88
-  Operation: new = old + 5
-  Test: divisible by 19
-    If true: throw to monkey 7
-    If false: throw to monkey 1
-
-Monkey 7:
-  Starting items: 72, 54, 63, 80
-  Operation: new = old + 3
-  Test: divisible by 7
-    If true: throw to monkey 1
-    If false: throw to monkey 3
-";
-
 var res = 0;
 
-var monkeys = input
-    .Split("Monkey")
-    .Where(i => !string.IsNullOrWhiteSpace(i))
-    .Select(m => new Monkey(m))
-    .ToList();
+var input = @"abaaaaaccccccccccccccccccaaaaaaaaaaaaaccccaaaaaaaccccccccccccccccccccccccccccaaaaaa
+abaaaaaaccaaaacccccccccccaaaaaaaaacaaaacaaaaaaaaaacccccccccccccccccccccccccccaaaaaa
+abaaaaaacaaaaaccccccccccaaaaaaaaaaaaaaacaaaaaaaaaacccccccccccccaacccccccccccccaaaaa
+abaaaaaacaaaaaacccccccccaaaaaaaaaaaaaaccaaacaaaccccccccccccccccaacccccccccccccccaaa
+abccaaaccaaaaaacccaaaaccaaaaaaaaaaaaaccccaacaaacccccccccaacaccccacccccccccccccccaaa
+abcccccccaaaaaccccaaaacccccaaaaacccaaaccaaaaaaccccccccccaaaaccccccccccccccccccccaac
+abcccccccccaaaccccaaaacccccaaaaacccccccccaaaaaccccccccccklllllccccccccccccccccccccc
+abcccccccccccccccccaaccccccccaaccccccccaaaaaaaccccccccckklllllllcccccddccccaacccccc
+abaccccccccccccccccccccccccccaaccccccccaaaaaaaaccccccckkkklslllllcccddddddaaacccccc
+abacccccccccccccccccccccccccccccccaaaccaaaaaaaaccccccckkkssssslllllcddddddddacccccc
+abaccccccccccccccccccccccccccccccccaaaaccaaacaccccccckkksssssssslllmmmmmdddddaacccc
+abcccccccccccccccaaacccccccccccccaaaaaaccaacccccccccckkkssssusssslmmmmmmmdddddacccc
+abcccccccaaccccaaaaacccccccccccccaaaaaccccccaaaaaccckkkrssuuuussssqmmmmmmmmdddccccc
+abcccccccaaccccaaaaaacccccccaaccccaaaaacccccaaaaacckkkkrruuuuuussqqqqqqmmmmdddccccc
+abccccaaaaaaaacaaaaaacccccccaaaaccaaccaccccaaaaaacjkkkrrruuuxuuusqqqqqqqmmmmeeccccc
+abcaaaaaaaaaaacaaaaaccccccaaaaaacccccaaccccaaaaajjjjrrrrruuuxxuvvvvvvvqqqmmmeeccccc
+abcaacccaaaaccccaaaaaaacccaaaaacccacaaaccccaaaajjjjrrrrruuuxxxxvvvvvvvqqqmmeeeccccc
+abaaaaccaaaaacccccccaaaccccaaaaacaaaaaaaacccaajjjjrrrrtuuuuxxxyvyyyvvvqqqnneeeccccc
+abaaaaaaaaaaacccaaaaaaaccccaacaacaaaaaaaacccccjjjrrrttttuxxxxxyyyyyvvvqqnnneeeccccc
+abaaaaaaaccaacccaaaaaaaaacccccccccaaaaaaccccccjjjrrrtttxxxxxxxyyyyyvvvqqnnneeeccccc
+SbaaaaaacccccccccaaaaaaaaaccccccccaaaaacccccccjjjrrrtttxxxEzzzzyyyvvrrrnnneeecccccc
+abaaaaacccccccccccaaaaaaacccccccccaaaaaaccccccjjjqqqtttxxxxxyyyyyvvvrrrnnneeecccccc
+abaaacccccccccccaaaaaaaccaaccccccccccaaccaaaaajjjqqqttttxxxxyyyyyyvvrrrnnneeecccccc
+abaaacccccccccccaaaaaaaccaaacaaacccccccccaaaaajjjjqqqtttttxxyywyyyywvrrnnnfeecccccc
+abcaaacccccccaaaaaaaaaaacaaaaaaaccccccccaaaaaaciiiiqqqqtttxwyywwyywwwrrrnnfffcccccc
+abcccccccccccaaaaaaaaaaccaaaaaacccccccccaaaaaacciiiiqqqqttwwywwwwwwwwrrrnnfffcccccc
+abccccccccccccaaaaaacccaaaaaaaacccccccccaaaaaaccciiiiqqqttwwwwwswwwwrrrrnnfffcccccc
+abccccccccccccaaaaaacccaaaaaaaaacccccccccaaacccccciiiqqqtswwwwssssrrrrrroofffcccccc
+abccccccaaaaacaaaaaacccaaaaaaaaaaccccccccccccccccciiiqqqssswsssssssrrrrooofffaccccc
+abccccccaaaaacaaccaaccccccaaacaaacccccccccccccccccciiiqqssssssspoorrrooooofffaacccc
+abcccccaaaaaacccccccccccccaaacccccccccccccccccccccciiiqppssssspppooooooooffffaacccc
+abcccccaaaaaacccccccccccccaacccccccccccccccccccccccciipppppppppppoooooooffffaaccccc
+abcccccaaaaaaccccccccccccccccccccccccccccccccccccccciihppppppppgggggggggfffaaaccccc
+abccccccaaacccccccccccccccccccccccaccccccccccccccccchhhhpppppphggggggggggfaaaaccccc
+abaaaccccccccccccccccccccccaccccaaacccccccccccccccccchhhhhhhhhhgggggggggcaacccccccc
+abaaccaaaccaccccccccccccccaaacccaaacaacccaaaaacccccccchhhhhhhhhgaaccccccccccccccccc
+abaaacaaacaacccccccccaaaccaaaacaaaaaaaaccaaaaaccccccccchhhhhhaaaaacccccccccccccccca
+abaaaccaaaaaccccccccccaaacaaaaaaaacaaaaccaaaaaaccccccccccaaacccaaaacccccccccccaccca
+abcccaaaaaaccccccccccaaaaaaaaaaaaacaaaaccaaaaaaccccccccccaaaccccaaaccccccccccaaaaaa
+abcccaaaaaaaacccccccaaaaaaaaaaaaaaaaaccccaaaaaacccccccccccccccccccccccccccccccaaaaa
+abcccaacaaaaaccccccaaaaaaaaaaaaaaaaaaacccccaacccccccccccccccccccccccccccccccccaaaaa";
 
-var monkeysCounts = new long[monkeys.Count];
+var lines = input.Split('\n').Where(i => i.Length > 0).ToList();
 
-var lcm = monkeys.Aggregate(1, (acc, m) => acc * m.Test);
+int height = lines.Count;
+int width = lines[0].Length;
 
-for (var i = 0; i < 10_000; ++i)
+var maze = new int[height, width];
+var memoization = new int[height, width];
+
+(int, int) start = (0, 0),
+           end = (0, 0);
+
+for (var i = 0; i < height; i++)
 {
-    for (var index = 0; index < monkeys.Count; index++)
+    for (var j = 0; j < width; ++j)
     {
-        var m = monkeys[index];
-
-        monkeysCounts[index] += m.Items.Count;
-        while (m.Items.Count > 0)
+        memoization[i, j] = int.MaxValue;
+        maze[i, j] = lines[i][j];
+        switch (maze[i, j])
         {
-            var current = m.Items.Dequeue();
-            current = m.Operation(current);
-            current %= lcm;
-            // current /= 3;
-            if (current % m.Test == 0)
-                monkeys[m.TrueMonkey].Items.Enqueue(current);
-            else
-                monkeys[m.FalseMonkey].Items.Enqueue(current);
+            case 'S':
+                start = (i, j);
+                maze[i, j] = 'a';
+                break;
+            case 'E':
+                end = (i, j);
+                maze[i, j] = 'z';
+                break;
         }
     }
 }
 
-Console.WriteLine(monkeysCounts.OrderByDescending(i => i).Take(2).Aggregate((long)1, (acc, val) => acc * val));
+memoization[start.Item1, start.Item2] = 0;
 
+var queue = new Queue<(int, int)>();
+queue.Enqueue(start);
 
-internal class Monkey
+while (queue.TryDequeue(out var position))
 {
-    static readonly Regex StartingRegex = new Regex(@"Starting items: ([\d, ]*)");
-    static readonly Regex StartingNumbersRegex = new Regex(@"\d+");
-    static readonly Regex OperationRegex = new Regex(@"Operation: new = old ([*+]) (old|\d+)");
-    static readonly Regex TestRegex = new Regex(@"Test: divisible by (\d+)");
-    static readonly Regex TrueRegex = new Regex(@"If true: throw to monkey (\d+)");
-    static readonly Regex FalseRegex = new Regex(@"If false: throw to monkey (\d+)");
+    var x = position.Item1;
+    var y = position.Item2;
 
-    public Monkey(string input)
+    (int, int) nextPos;
+    
+    // up
+    if (x > 0)
     {
-        Items = new Queue<long>();
-        var startingItems = StartingRegex.Match(input);
-        var item = StartingNumbersRegex.Matches(startingItems.Value);
-        foreach (Match i in item)
+        nextPos = (x - 1, y);
+        
+        if (maze[nextPos.Item1, nextPos.Item2] - maze[x, y] <= 1)
         {
-            Items.Enqueue(int.Parse(i.Value));
+            if (memoization[nextPos.Item1, nextPos.Item2] > memoization[x, y] + 1)
+            {
+                memoization[nextPos.Item1, nextPos.Item2] = memoization[x, y] + 1;
+                if(!queue.Contains(nextPos))
+                    queue.Enqueue(nextPos);
+            }
         }
-
-        Test = int.Parse(TestRegex.Match(input).Groups[1].Value);
-        TrueMonkey = int.Parse(TrueRegex.Match(input).Groups[1].Value);
-        FalseMonkey = int.Parse(FalseRegex.Match(input).Groups[1].Value);
-
-        var parts = OperationRegex.Match(input);
-        Operation = current =>
+    }
+    
+    // down
+    if (x < height - 1)
+    {
+        nextPos = (x + 1, y);
+        
+        if (maze[nextPos.Item1, nextPos.Item2] - maze[x, y] <= 1)
         {
-            var part1 = current;
-            var part2 = parts.Groups[2].Value == "old" ? current : int.Parse(parts.Groups[2].Value);
-
-            if (parts.Groups[1].Value == "+")
-                return part1 + part2;
-
-            return part1 * part2;
-        };
+            if (memoization[nextPos.Item1, nextPos.Item2] > memoization[x, y] + 1)
+            {
+                memoization[nextPos.Item1, nextPos.Item2] = memoization[x, y] + 1;
+                if(!queue.Contains(nextPos))
+                    queue.Enqueue(nextPos);
+            }
+        }
     }
 
-    public Queue<long> Items { get; }
-    public Func<long, long> Operation { get; }
-
-    public int Test { get; }
-    public int TrueMonkey { get; }
-    public int FalseMonkey { get; }
+    // left
+    if (y > 0)
+    {
+        nextPos = (x, y - 1);
+        
+        if (maze[nextPos.Item1, nextPos.Item2] - maze[x, y] <= 1)
+        {
+            if (memoization[nextPos.Item1, nextPos.Item2] > memoization[x, y] + 1)
+            {
+                memoization[nextPos.Item1, nextPos.Item2] = memoization[x, y] + 1;
+                if(!queue.Contains(nextPos))
+                    queue.Enqueue(nextPos);
+            }
+        }
+    }
+    
+    // right
+    if (y < width - 1)
+    {
+        nextPos = (x, y + 1);
+        
+        if (maze[nextPos.Item1, nextPos.Item2] - maze[x, y] <= 1)
+        {
+            if (memoization[nextPos.Item1, nextPos.Item2] > memoization[x, y] + 1)
+            {
+                memoization[nextPos.Item1, nextPos.Item2] = memoization[x, y] + 1;
+                if(!queue.Contains(nextPos))
+                    queue.Enqueue(nextPos);
+            }
+        }
+    }
 }
 
 
-// Console.WriteLine(res);
+Console.WriteLine(memoization[end.Item1, end.Item2]);
